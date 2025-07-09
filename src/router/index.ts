eb46e1342b4fc00169117a3e154bf572f804a5c2
David-Deve <sopheaktra26@gmail.com>
2025-07-09 14:03:20 +0700

    fix login route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import VueCookies from 'vue-cookies'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,6 +10,7 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: LoginView,
+      meta: { requiresNoAuth: true },
     },
     {
       path: '/home',
@@ -18,10 +20,12 @@ const router = createRouter({
     },
   ],
 })
+
 router.beforeEach((to, from, next) => {
   const isAuth = VueCookies.get('storepos')
   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
   const requiresNoAuth = to.matched.some((record) => record.meta.requiresNoAuth)
+
   if (requiresAuth && !isAuth) {
     next('/')
   } else if (requiresNoAuth && isAuth) {
@@ -30,4 +34,5 @@ router.beforeEach((to, from, next) => {
     next()
   }
 })
+
 export default router
